8d1936f3f466404a0ef776cb21d47c6c78e3517b
Vipin Dane <vipindane1@gmail.com>
2023-02-18 17:13:52 +0530

    Cross Origin added

--- SastaBasta/src/main/java/com/sastabasta/controller/ProductController.java
+++ SastaBasta/src/main/java/com/sastabasta/controller/ProductController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import com.sastabasta.service.WishlistService;
 
 @RestController
 @RequestMapping("/product")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
 	
 	@Autowired
--- SastaBasta/src/main/java/com/sastabasta/controller/WishlistController.java
+++ SastaBasta/src/main/java/com/sastabasta/controller/WishlistController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import com.sastabasta.service.WishlistService;
 
 @RestController
 @RequestMapping("/wishlist")
+@CrossOrigin(origins = "http://localhost:3000")
 public class WishlistController {
 	@Autowired
 	WishlistService wishlistService;
